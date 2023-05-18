01840067796547669ba9edd6c5a3be5d93e37216
“SueldoMariano” <sueldo.011.ms@gmail.com>
2023-05-18 17:44:10 -0300

    Primer punto desarrollado

---
+++ practico6/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+
+int b;
+
+a=10;
+
+b=a;
+
+Console.WriteLine("Valor de a: ",+a);
+Console.WriteLine("Valor de b: ",+b);
